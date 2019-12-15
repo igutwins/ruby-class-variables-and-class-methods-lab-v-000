3c11020e6b584ebd124421a50fe2b2d773314d3f
Ian Gutwinski <iangutwinski@gmail.com>
2019-12-15 16:08:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
 attr_accessor :name, :artist, :genre
-@@songs = 0
+@@count = 0
 @@artists = []
 @@genres = []
 
@@ -8,7 +8,7 @@ def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
-  @@songs += 1
+  @@count += 1
 end
 
 def self.count
